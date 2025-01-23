5886f87601d9f7a3d25739234a669b0f32290b52
Nguyễn Phạm Gia Bảo <hello@giabao06.xyz>
2025-01-23 14:22:39 +0700

    Import cleanup

--- src/main/java/xyz/giabao06/jwtest/TestApp.java
+++ src/main/java/xyz/giabao06/jwtest/TestApp.java
@@ -4,19 +4,13 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import xyz.giabao06.jwtest.db;
 
-import javax.sql.DataSource;
-import java.io.PrintWriter;
 import java.sql.*;
 
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @SpringBootApplication
--- src/main/java/xyz/giabao06/jwtest/db.java
+++ src/main/java/xyz/giabao06/jwtest/db.java
@@ -2,13 +2,11 @@ package xyz.giabao06.jwtest;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
-import org.springframework.context.annotation.Bean;
 
-import javax.sql.DataSource;
 import java.sql.*;
 
 public class db{
-    private static HikariConfig cfg = new HikariConfig();
+    private static final HikariConfig cfg = new HikariConfig();
     private static HikariDataSource ds;
     public db(String dbname){
         cfg.setJdbcUrl(String.format("jdbc:sqlite:%s", dbname));
